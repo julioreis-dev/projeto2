b3dfb2f52e258fdcc9fb95d33c4c8969ca06e2ff
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-10-21 23:10:49 -0300

    Add files via upload

--- indisp_inverter.py
+++ indisp_inverter.py
@@ -9,23 +9,19 @@ class Inverterfilter:
 
     def filter(self):
         wb = Workbook()
+        number = 0
         for n in self.files:
             label = n[-22:-4]
             df = pd.read_csv(n)
             df_read = df.loc[df['COMS STATUS'] == 255]
             del df_read['ACTIVE POWER']
             if not df_read.empty:
-                self.createsheets(wb, label)
+                wb.create_sheet(label)
                 ws = wb.get_sheet_by_name(label)
                 df_read = pd.DataFrame(df_read, columns=['EQUIPAMENTO', 'timestamp', 'COMS STATUS'])
                 df_read = df_read.fillna(label)
                 for r in dataframe_to_rows(df_read, index=False, header=True):
                     ws.append(r)
                 ws['f2'] = df_read.shape[0]
-                    # ws.cell(row=2, column=7).value = df_read.shape[0]
+                number += 1
         wb.save('teste1.xlsx')
-
-
-    def createsheets(self, wb,label):
-        wb.create_sheet(label)
-        # ws = wb.get_sheet_by_name(label)

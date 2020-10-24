a56071e2fc804b23893ec970011534f75bd36e0c
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-10-23 22:51:07 -0300

    Add files via upload

--- indisp_inverter.py
+++ indisp_inverter.py
@@ -12,6 +12,10 @@ class InverterFilter:
         self.minute = minute
 
     def filter(self):
+        listequip = []
+        listocurr = []
+        listtime = []
+        listporc = []
         wb = Workbook()
         number = 0
         for n in self.files:
@@ -20,10 +24,20 @@ class InverterFilter:
             df_read = df.loc[df['COMS STATUS'] == 255]
             del df_read['ACTIVE POWER']
             if not df_read.empty:
-                self.formatcontent(wb, label, df_read)
+                general_datas = self.formatcontent(wb, label, df_read)
+                listequip.append(general_datas[0])
+                listocurr.append(general_datas[1])
+                listtime.append(general_datas[2])
+                listporc.append(general_datas[3])
                 number += 1
-        print('{} inversores possuem dados de indisponibilidade.'.format(number))
+        wt = wb.get_sheet_by_name('Sheet')
+        obj = {'Equipamento': listequip, 'Ocorrências': listocurr, 'Tempo': listtime, '% indisp': listporc}
+        df_report = pd.DataFrame(data=obj)
+        for r in dataframe_to_rows(df_report, index=False, header=True):
+            wt.append(r)
+        print('{} inversores possuem registro de indisponibilidade.'.format(number))
         wb.save('teste1.xlsx')
+        wb.close()
 
     def formatcontent(self, wb, label, df_read):
         wb.create_sheet(label)
@@ -31,22 +45,34 @@ class InverterFilter:
         df_read = pd.DataFrame(df_read, columns=['timestamp', 'COMS STATUS'])
         for r in dataframe_to_rows(df_read, index=False, header=True):
             ws.append(r)
-        self.formatsheet(ws, df_read, label)
+        datas = self.recorddata(ws, label, df_read)
+        self.formatsheet(ws)
+        return datas
 
-    def formatsheet(self, ws, df_read, label):
-        self.ocurr = df_read.shape[0]
+    def formatsheet(self, ws):
         ws['e2'] = 'Equipamento:'
         ws['e3'] = 'Ocorrencias:'
         ws['e4'] = 'Tempo total:'
-        ws['e5'] = 'Indisponível no mês:'
-        ws['f2'] = '{}'.format(label)
-        ws['f3'] = self.ocurr
-        ws['f4'] = self.calctime()
-        ws['f5'] = '{}%'.format(round(self.calcporc(), 2))
+        ws['e5'] = 'Indisp no mês:'
         ws['f2'].alignment = Alignment(horizontal='left')
         ws['f3'].alignment = Alignment(horizontal='left')
         ws['f4'].alignment = Alignment(horizontal='left')
         ws['f5'].alignment = Alignment(horizontal='left')
+
+    def recorddata(self, ws, label, df_read):
+        self.ocurr = df_read.shape[0]
+        sheet_name = label
+        ocurr =  self.ocurr
+        calctime = self.calctime()
+        porcen = round(self.calcporc(), 2)
+        ws['f2'] = '{}'.format(sheet_name)
+        ws['f3'] = ocurr
+        ws['f4'] = calctime
+        ws['f5'] = '{}%'.format(porcen)
+        return sheet_name, ocurr, calctime, porcen
+
+    def listdata(self):
+        pass
 
     def calctime(self):
         self.hr = (self.ocurr // 4)

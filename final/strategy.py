023df763a3e214d836c545f78a865a8221b20de1
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-10-26 23:30:04 -0300

    Update strategy.py

--- final/strategy.py
+++ final/strategy.py
@@ -4,7 +4,7 @@ from openpyxl.styles import Font, Alignment
 
 
 class Strategy:
-    def __init__(self, files, ocurr=0, hr=0, minute=0):
+    def __init__(self, files, ocurr=0, hr=0, minute=0):#revisar construtor
         self.files = files
         self.ocurr = ocurr
         self.hr = hr

4e3b1d65e8dfbf9158b90dfecc99fd1355c843ce
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-10-26 21:26:46 -0300

    Add files via upload

--- final/Strategyc_stringbox.py
+++ final/Strategyc_stringbox.py
@@ -1,29 +1,29 @@
 import pandas as pd
 from openpyxl import Workbook
-from strategy import Strategy as st
+from strategy import Strategy
 
 
-class BoxFilter(st):
+class BoxFilter(Strategy):
 
     def __init__(self, files):
-        super().__init__(self, files, hr=0, minute=0, listequip=None, listocurr=None, listtime=None, listporc=None)
+        super().__init__(files)
 
     def filter(self):
         wb = Workbook()
         number = 0
-        total_files = len(self.ocurr)
+        total_files = len(self.files)
         dataslist = []
-        for n in self.ocurr:
+        for n in self.files:
             label = n[-28:-4]
             df = pd.read_csv(n)
             df_read = df.loc[df['COMS STATUS'] == 255]
             del df_read['Current']
             del df_read['Power']
             if not df_read.empty:
-                alldatas = st.formatcontent(self, wb, label, df_read)
-                dataslist = st.listdata(self, alldatas[0], alldatas[1], alldatas[2], alldatas[3])
+                alldatas = self.formatcontent(wb, label, df_read)
+                dataslist = list(self.listdata(alldatas[0], alldatas[1], alldatas[2], alldatas[3]))
                 number += 1
-        st.report(wb, dataslist[0], dataslist[1], dataslist[2], dataslist[3])
+        self.report(wb, dataslist[0], dataslist[1], dataslist[2], dataslist[3])
         wb.save('teste2.xlsx')
         wb.close()
-        print('{}/{} Stringsbox possuem registro de indisponibilidade.'.format(number, total_files))
+        print('{}/{} Stringsbox possuem registro de indisponibilidade.\n'.format(number, total_files))
